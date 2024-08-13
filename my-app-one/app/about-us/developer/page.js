2b66f85ea30ada8ebf930e62a7b5745b756b6cc4
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-13 22:30:12 +0530

    Nested routes using nextJS

---
+++ my-app-one/app/about-us/developer/page.js
@@ -0,0 +1,11 @@
+const Developer = () => {
+    return (
+      <div>
+        <h1>About Us: Developer</h1>
+        <p>This page is about the developer, Piyush.</p>
+      </div>
+    );
+  };
+  
+  export default Developer;
+  

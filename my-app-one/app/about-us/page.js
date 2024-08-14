0ed387bb2ed1cc18abc07973b2abd69b085c3235
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-14 18:44:40 +0530

    Updated AboutUs for clickable links using <Link>

--- my-app-one/app/about-us/page.js
+++ my-app-one/app/about-us/page.js
@@ -1,8 +1,24 @@
+import Link from 'next/link';
+
+const developers = [
+  { id: 1, name: 'Yash' },
+  { id: 2, name: 'Vaibhav' },
+  { id: 3, name: 'Suresh' },
+];
+
 const AboutUs = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <ul>
+        {developers.map(dev => (
+          <li key={dev.id}>
+            <Link href={`/about-us/${dev.id}`}>{dev.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return(
-        <h1>This is about us page</h1>
-    )
-}
-
-export default AboutUs
+export default AboutUs;
